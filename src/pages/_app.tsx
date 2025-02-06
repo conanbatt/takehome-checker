a8cfd03c6202955b0ef80dd1edef7d73d98c9640
mdecarlo <mati.dc89@gmail.com>
2025-02-05 23:44:58 -0300

    add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import AppQueryProvider from "@components/QueryClientProvider";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AppQueryProvider>
+        <Component {...pageProps} />
+      </AppQueryProvider>
+    </SessionProvider>
+  );
 }
