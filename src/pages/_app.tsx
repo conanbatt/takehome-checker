585ce2d06488d3e8ea43ef01373c2b2aa4f7ce8e
mdecarlo <mati.dc89@gmail.com>
2025-02-09 23:54:00 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 
+import '@/styles/globals.css';
 import AppQueryProvider from "@components/QueryClientProvider";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
