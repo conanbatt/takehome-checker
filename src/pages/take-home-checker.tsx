40716a6595be7e9f5b7461074c5ab84d15f34df9
mdecarlo <mati.dc89@gmail.com>
2025-02-15 11:09:41 -0300

    feat: Sort repositories by most recent update first.

--- src/pages/take-home-checker.tsx
+++ src/pages/take-home-checker.tsx
@@ -12,8 +12,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     try {
         const octokit = new Octokit({ auth: session.accessToken });
-        const { data: repos } = await octokit.request("GET /user/repos", { visibility: "all" });
-
+        const { data: repos } = await octokit.request("GET /user/repos", {
+            visibility: "all",
+            per_page: 100,
+            sort: "updated",
+            direction: "desc",
+        });
         return { props: { repos: repos as Repo[], isAuthenticated: true } };
     } catch (error) {
         console.error("Error fetching repos:", error);
