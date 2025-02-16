24e8b0b004fbeb1cd48293c171f0e54a2a449eed
mdecarlo <mati.dc89@gmail.com>
2025-02-16 00:27:45 -0300

    feat: Add Take home checker footer

--- src/components/RepoAnalysis.tsx
+++ src/components/RepoAnalysis.tsx
@@ -7,6 +7,7 @@ import ReadmeViewer from "./ReadmeViewer";
 import LoadingBanner from "./LoadingBanner";
 import { motion } from "framer-motion";
 import useLoadingMessage from "@/hooks/useLoadingMessage";
+import AppInfoWithVideo from "./AppInfoWithVideo";
 
 interface RepoAnalysisProps {
   repos: Repo[];
@@ -16,7 +17,7 @@ interface RepoAnalysisProps {
 export default function RepoAnalysis({ repos, token }: RepoAnalysisProps) {
   const [selectedRepo, setSelectedRepo] = useState<Repo | null>(null);
 
-  const { data, isLoading, isError, error, refetch } = useProjectAnalysis(selectedRepo, token);
+  const { data, isSuccess, isLoading, isError, error, refetch } = useProjectAnalysis(selectedRepo, token);
   const loadingMessage = useLoadingMessage(isLoading);
 
   const {
@@ -58,21 +59,25 @@ export default function RepoAnalysis({ repos, token }: RepoAnalysisProps) {
           Error: {error instanceof Error ? error.message : "An error occurred"}
         </p>
       )}
+      {isSuccess &&
+        (<>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="grid grid-cols-2 gap-6 mt-6 pb-6"
+          >
+            <div className="">
+              <ReadmeViewer markdown={readme} />
+            </div>
+            <div className="">
+              {analysis && <ProjectAnalysis {...analysis} />}
+            </div>
+          </motion.div>
 
-      {readme && (
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="grid grid-cols-2 gap-6 mt-6 pb-6"
-        >
-          <div className="">
-            <ReadmeViewer markdown={readme} />
-          </div>
-          <div className="">
-            {analysis && <ProjectAnalysis {...analysis} />}
-          </div>
-        </motion.div>
-      )}
+          <hr className="w-full my-8 lg:col-span-2" />
+          <AppInfoWithVideo appName="Take Home Checker" videoUrl="https://www.youtube.com/embed/aeyAG2DPWz0?si=IeayRmBlEt0Iv2-T" />
+        </>
+        )}
     </div>
   );
 }
